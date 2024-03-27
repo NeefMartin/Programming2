43b3c9c914361ef0f579e0f8c1cd5e0b24a392ce
Leolyciia <leonie.hoekstra10@gmail.com>
2024-03-27 12:35:29 +0100

    Removal of .cpp & .hpp files
    I have removed all the .cpp and .hpp files that I've merged with the main.cpp

--- pacman/PacmanLib (1)/PacmanLib/src/GameBoard.cpp
+++
@@ -1,9 +0,0 @@
-// GameBoard.cpp
-#include "GameBoard.hpp"
-#include "UI.hpp"
-
-GameBoard::GameBoard(const std::vector<std::vector<int>>& map) : map(map) {}
-
-void GameBoard::drawBoard(UI& ui) const {
-    ui.drawBackground(map);
-}
